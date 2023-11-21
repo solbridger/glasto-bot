2895d11e39147f4ef8f1bb33db4bff1ad4f95c9e
Alessio <alessio.livolsi@protonmail.com>
2023-11-21 20:04:17 +0000

    Enhanced log.js with detailed comments for improved code readability

--- log.js
+++ log.js
@@ -1,23 +1,37 @@
 const winston = require('winston');
 
-// Logger configuration
+/**
+ * Configures and creates a Winston logger.
+ *
+ * Winston is used for logging information and errors during the application's runtime.
+ * This configuration sets up a logger that outputs logs in JSON format with a timestamp,
+ * log level, tab identifier (if provided), and the log message.
+ *
+ * The logs are output to both the console and a file named 'glasto.log'.
+ */
 const logger = winston.createLogger({
+    // Combining multiple log formats.
     format: winston.format.combine(
+        // Adds a timestamp to each log message.
         winston.format.timestamp(),
+        // Formats the log as a JSON.
         winston.format.json(),
+        // Custom function to format the log message.
         winston.format.printf((info) => {
             return JSON.stringify({
                 timestamp: info.timestamp,
                 level: info.level,
+                // Custom attribute for logging the tab number.
                 tab: info.tab,
                 message: info.message
             });
         })
     ),
+    // Defining the transports for logging: console and file.
     transports: [
         new winston.transports.Console(),
         new winston.transports.File({ filename: 'glasto.log' })
     ]
 });
-
+// Exporting the configured logger for use in other modules.
 module.exports = logger;
