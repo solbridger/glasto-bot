fa0cdaccdd11745c8f7b92f95c748d94e5cec747
Alessio <alessio.livolsi@protonmail.com>
2023-11-21 20:12:09 +0000

    Refactor: Refactored main.js for code consistency with semicolons, quotes, and formatting

--- main.js
+++ main.js
@@ -1,32 +1,18 @@
 #!/usr/bin/env node
 
 const winston = require('winston');
-const argv = require('yargs').argv
+const argv = require('yargs').argv;
 const readline = require('readline');
 const fs = require('fs');
 const util = require('util');
-const puppeteer = require("puppeteer-extra")
-const pluginStealth = require("puppeteer-extra-plugin-stealth")
-puppeteer.use(pluginStealth())
+const puppeteer = require('puppeteer-extra');
+const pluginStealth = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(pluginStealth());
 const readFile = util.promisify(fs.readFile);
 process.setMaxListeners(Infinity);
 
 // Import the logger from log.js
 const logger = require('./log');
-
-
-/*
-Excuse my node
-
-To-do
-High priority:
-
-Low priority:
-    - Extract inner text from actual glastonbury page using same method so we can match better. The 2019 glasto page exists on the selenium page.
-    - Add randomness to refresh time
-Other:
-    - It would be nice to scale this so it's not just maxing out one device. How though? Possibly containerize and give each container its own VPN
-*/
 
 class Puppets {
     constructor(url, rateLimitPerMinute, registrationPageInnerText) {
@@ -41,9 +27,9 @@ class Puppets {
 
     setPaused(paused) {
         if (paused) {
-            logger.info("Pausing operation. Tabs wills finish their current page load.");
+            logger.info('Pausing operation. Tabs will finish their current page load.');
         } else {
-            logger.info("Resuming operation.");
+            logger.info('Resuming operation.');
         }
         this.paused = paused;
     }
@@ -75,14 +61,13 @@ class Puppets {
     }
 
     calculateSimilarity(retrievedText, desiredText) {
-        const retrievedTextTokens = retrievedText.replace(/(\r\n|\n|\r)/gm, "").toLowerCase().split(" ");
-        const desiredTextTokens = desiredText.replace(/(\r\n|\n|\r)/gm, "").toLowerCase().split(" ");
+        const retrievedTextTokens = retrievedText.replace(/(\r\n|\n|\r)/gm, '').toLowerCase().split(' ');
+        const desiredTextTokens = desiredText.replace(/(\r\n|\n|\r)/gm, '').toLowerCase().split(' ');
         let countOfMatchingWords = 0;
 
-        // How could this be improved?
         for (let i = 0; i < desiredTextTokens.length; i++) {
             if (retrievedTextTokens.includes(desiredTextTokens[i])) {
-                countOfMatchingWords = countOfMatchingWords + 1;
+                countOfMatchingWords++;
             }
         }
 
@@ -91,7 +76,6 @@ class Puppets {
     }
 
     async getHighestScoringTabIndex() {
-        // Get the tab with highest score. Return the first found if multiple exist with same score
         let highestScorer = null;
         for (let i = 0; i < this.tabs.length; i++) {
             if (highestScorer == null) {
@@ -105,65 +89,43 @@ class Puppets {
         return highestScorer;
     }
 
-    // TODO: tidy method
     async loadPagesAtRate() {
         while (true) {
             for (let i = 0; i < this.tabs.length; i++) {
-                while (this.paused == true) {
+                while (this.paused === true) {
                     await this.sleep(10);
                 }
-                //Don't reload the page we think is most similar, unless it's score is 0 (which it starts off with)
-                if (i != await this.getHighestScoringTabIndex() || await this.tabs[i].getSimilarityScore() == -1) {
-                    if (await this.tabs[i].getReady() == true) {
-
-                        logger.info({
-                            tab: i,
-                            message: `Loading page`
-                        });
+                if (i !== await this.getHighestScoringTabIndex() || await this.tabs[i].getSimilarityScore() === -1) {
+                    if (await this.tabs[i].getReady() === true) {
+                        logger.info({ tab: i, message: 'Loading page' });
                         this.tabs[i].loadPage().then(async page => {
-                            logger.info({
-                                tab: i,
-                                message: `Loaded page in ${Date.now() - this.tabs[i].getStartTime()}ms`
-                            });
-
+                            logger.info({ tab: i, message: `Loaded page in ${Date.now() - this.tabs[i].getStartTime()}ms` });
                             await this.tabs[i].getInnerHtmlTextOfAllElements().then(async pageInnerHtmlText => {
                                 const similarityScore = await this.calculateSimilarity(pageInnerHtmlText, this.registrationPageInnerText);
                                 await this.tabs[i].setSimilarityScore(similarityScore);
-                                logger.info({
-                                    tab: i,
-                                    message: `${similarityScore.toFixed(2)}% similarity found`
-                                });
+                                logger.info({ tab: i, message: `${similarityScore.toFixed(2)}% similarity found` });
 
-                                //Hard coded this pause as results from the coach tickets run showed the page we want has a similarity score of 91
                                 if (similarityScore > this.similarityThreshold) {
                                     this.paused = true;
-                                    logger.info({
-                                        tab: i,
-                                        message: `Paused operation as page with > ${this.similarityThreshold}% found`
-                                    });
+                                    logger.info({ tab: i, message: `Paused operation as page with > ${this.similarityThreshold}% found` });
                                 }
                                 const highestScoringTab = await this.getHighestScoringTabIndex();
-                                if(highestScoringTab != this.lastHighScorer) {
+                                if (highestScoringTab !== this.lastHighScorer) {
                                     this.lastHighScorer = highestScoringTab;
                                     await this.tabs[highestScoringTab].bringToFront();
                                 }
                                 await this.tabs[i].setReady(true);
                             });
                         }).catch(async error => {
-                            logger.error({
-                                tab: i,
-                                message: error
-                            });
+                            logger.error({ tab: i, message: error });
                             await this.tabs[i].setReady(true);
                         });
 
-                        //Wait until enough time has passed before loading next tab so we don't break the rate limit
                         const finishTime = Date.now();
                         if (finishTime - this.tabs[i].getStartTime() < this.refreshRateInMs) {
                             await this.sleep(this.refreshRateInMs - (finishTime - this.tabs[i].getStartTime()));
                         }
                     } else {
-                        // I've added a sleep here as when there are no pages ready it will freeze up
                         await this.sleep(10);
                     }
                 }
@@ -194,38 +156,36 @@ class Tab {
     setReady(ready) {
         this.ready = ready;
     }
+
     getSimilarityScore() {
         return this.similarityScore;
     }
+
     getStartTime() {
         return this.startTime;
-
     }
+
     setSimilarityScore(similarityScore) {
         this.similarityScore = similarityScore;
     }
 
     async bringToFront() {
-        this.page.bringToFront();
+        await this.page.bringToFront();
     }
 
     async initialiseTab() {
-        logger.info("Spawning new tab")
+        logger.info('Spawning new tab');
         this.browser = await puppeteer.launch({
             headless: false,
-            args: ["--disable-gpu", '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage'
-            ]
+            args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
         });
         const pages = await this.browser.pages();
-
         this.page = pages.pop();
         this.ready = true;
     }
 
     async close() {
-        await this.browser.close()
+        await this.browser.close();
         return await this.page.close();
     }
 
@@ -239,13 +199,12 @@ class Tab {
     }
 
     async getInnerHtmlTextOfAllElements() {
-        let innerHtmlTextOfAllElements = "";
+        let innerHtmlTextOfAllElements = '';
         const options = await this.page.$$('body *');
         for (const option of options) {
-
             const label = await this.page.evaluate(el => el.innerText, option);
-            if (label != undefined && label.length > 0) {
-                innerHtmlTextOfAllElements = innerHtmlTextOfAllElements + label.trim() + " ";
+            if (label !== undefined && label.length > 0) {
+                innerHtmlTextOfAllElements += label.trim() + ' ';
             }
         }
         return innerHtmlTextOfAllElements;
@@ -255,12 +214,11 @@ class Tab {
         const result = await this.page.evaluate(selector);
         return result || '';
     }
-
 }
 
 function parseArgs() {
     if (!(argv['site'] && argv['rate-limit'] && argv['max-tabs'])) {
-        log.info(`Usage:\nnode main.js --site=\"localhost:3000\" --rate-limit=60 --max-tabs=10`);
+        logger.info('Usage:\nnode main.js --site="localhost:3000" --rate-limit=60 --max-tabs=10');
         process.exit(0);
     }
     return argv;
@@ -272,11 +230,11 @@ async function readFileAsString(filePath) {
 
 async function getRegistrationPageInnerText() {
     if (argv['test'] && argv['test'] !== 'false') {
-        return await readFileAsString("resources/test.txt").then(data => {
+        return await readFileAsString('resources/test.txt').then(data => {
             return data.toString();
         });
     } else {
-        return await readFileAsString("resources/live.txt").then(data => {
+        return await readFileAsString('resources/live.txt').then(data => {
             return data.toString();
         });
     }
@@ -285,16 +243,14 @@ async function getRegistrationPageInnerText() {
 async function run() {
     parseArgs();
     const registrationPageInnerText = await getRegistrationPageInnerText();
-
     const tabs = new Puppets(argv['site'], argv['rate-limit'], registrationPageInnerText);
 
-    // Pause/resume by pressing enter
     readline.emitKeypressEvents(process.stdin);
     process.stdin.on('keypress', (str, key) => {
         if (key.ctrl && key.name === 'c') {
             tabs.closeTabs();
             process.exit(0);
-        } else if (key.name == 'enter') {
+        } else if (key.name === 'enter') {
             tabs.setPaused(!tabs.getPaused());
         }
     });
@@ -302,4 +258,5 @@ async function run() {
     await tabs.initializeTabs(argv['max-tabs']);
     await tabs.loadPagesAtRate();
 }
+
 run();
